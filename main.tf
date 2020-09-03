b647b611d349fcdee691d197029abe70fd02063e
Dave Parker <dparker@scalyr.com>
2020-09-02 23:37:10 -0700

    Use forked scalyr null-label module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/scalyr/terraform-null-label.git?ref=tags/0.16.0-scalyr"
   namespace   = var.namespace
   stage       = var.stage
   environment = var.environment
