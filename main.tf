b0533a158280f3377546a5ae69c973ea1177c86f
Amanibhavam <iam@defn.sh>
2021-02-25 08:58:46 -0800

    upgrade to tf 0.14

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source      = "git::https://github.com/scalyr/terraform-null-label.git?ref=tags/0.16.0-scalyr"
+  source      = "git::https://github.com/scalyr/terraform-null-label.git?ref=tf-0.14"
   namespace   = var.namespace
   stage       = var.stage
   environment = var.environment
