b6ddb4c204219697dd248b2fdd19604369d4449a
Amanibhavam <iam@defn.sh>
2021-02-25 09:09:50 -0800

    less provider restrictions

--- versions.tf
+++ versions.tf
@@ -2,19 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.0"
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
